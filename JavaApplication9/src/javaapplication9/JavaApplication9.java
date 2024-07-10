046f7e71982df38fc46afacb460ed1b9e5aec6d1
Diogo Carneiro <didicarneiro2022@gmail.com>
2024-07-10 11:39:36 -0300

    add projeto raso

---
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication9;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Diogo
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int n = 2;
+        int m = 2;
+        int r = n+m;
+        
+        JOptionPane.showMessageDialog(null, n+"+"+m+"="+r);
+        
+        
+    }
+    
+}
